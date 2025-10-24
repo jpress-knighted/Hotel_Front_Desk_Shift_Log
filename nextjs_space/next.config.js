585edcee4dc7c38591d68324348b2ad7ab8539da
jpress-knighted <jpress@knighted.com>
2025-10-24 10:37:55 -0500

    build patch

--- nextjs_space/next.config.js
+++ nextjs_space/next.config.js
@@ -1,12 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
